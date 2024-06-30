c188c6c44d8e86644b713827847b4f26d2bf5297
xchillz <cendaaal@tutanota.com>
2024-06-30 02:54:41 -0400

    feat: initial commit

---
+++ src/pocketmine/network/protocol/ChangeDimensionPacket.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ *
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author iTX Technologies
+ * @link https://itxtech.org
+ *
+ */
+
+namespace pocketmine\network\protocol;
+
+#include <rules/DataPacket.h>
+
+class ChangeDimensionPacket extends DataPacket{
+	const NETWORK_ID = Info::CHANGE_DIMENSION_PACKET;
+
+	const DIMENSION_NORMAL = 0;
+	const DIMENSION_NETHER = 1;
+
+	public $dimension;
+
+	public $x;
+	public $y;
+	public $z;
+
+	public function decode(){
+
+	}
+
+	public function encode(){
+		$this->reset();
+		$this->putByte($this->dimension);
+		$this->putFloat($this->x);
+		$this->putFloat($this->y);
+		$this->putFloat($this->z);
+		$this->putByte(0);
+	}
+
+}
---
+++ src/pocketmine/network/protocol/MoveEntityPacket.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
+*/
+
+namespace pocketmine\network\protocol;
+
+#include <rules/DataPacket.h>
+
+
+class MoveEntityPacket extends DataPacket{
+	const NETWORK_ID = Info::MOVE_ENTITY_PACKET;
+
+	public $eid;
+	public $x;
+	public $y;
+	public $z;
+	public $yaw;
+	public $headYaw;
+	public $pitch;
+
+	public function decode(){
+		$this->eid = $this->getLong();
+		$this->x = $this->getFloat();
+		$this->y = $this->getFloat();
+		$this->z = $this->getFloat();
+		$this->pitch = $this->getByte()*(360.0/256);
+		$this->yaw = $this->getByte()*(360.0/256);
+		$this->headYaw = $this->getByte()*(360.0/256);
+	}
+
+	public function encode(){
+		$this->reset();
+		$this->putLong($this->eid);
+		$this->putFloat($this->x);
+		$this->putFloat($this->y);
+		$this->putFloat($this->z);
+		$this->putByte($this->pitch/(360.0/256));
+		$this->putByte($this->yaw/(360.0/256));
+		$this->putByte($this->headYaw/(360.0/256));
+	}
+}
