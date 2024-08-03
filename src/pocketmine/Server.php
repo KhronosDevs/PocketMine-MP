093fb596d8db9913df6e8b3cdc1102f63070c3a4
xchillz <147117006+xchillz@users.noreply.github.com>
2024-08-03 13:01:34 -0400

    perf: load nether only on-demand

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1903,14 +1903,6 @@ class Server{
 				return;
 			}
 
-			if($this->netherEnabled){
-				if(!$this->loadLevel($this->netherName)){
-					//$this->logger->info("正在生成地狱 ".$this->netherName);
-					$this->generateLevel($this->netherName, time(), Generator::getGenerator("nether"));
-				}
-				$this->netherLevel = $this->getLevelByName($this->netherName);
-			}
-
 			if($this->getProperty("ticks-per.autosave", 6000) > 0){
 				$this->autoSaveTicks = (int) $this->getProperty("ticks-per.autosave", 6000);
 			}
