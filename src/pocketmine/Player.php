d219b95d7badfdf99bd36175fbff846df8a6f670
xchillz <147117006+xchillz@users.noreply.github.com>
2024-08-03 14:11:15 -0400

    fix: CPU/mem leak when teleporting with movement checks disabled

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -4061,6 +4061,8 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 			$pk->eid = 0;
 			$this->dataPacket($pk);
 		}
+
+		$this->newPosition = null;
 	}
 
 	protected function checkChunks(){
