f596f1b1712c76b8eb37434d20a336e8e9457960
xchillz <147117006+xchillz@users.noreply.github.com>
2024-08-03 16:35:05 -0400

    fix(player): accuracy of spam calculation

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2382,7 +2382,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 				$newPos = new Vector3($packet->x, $packet->y - $this->getEyeHeight(), $packet->z);
 
-				if($newPos->distanceSquared($this) < 0.01 and ($packet->yaw % 360) === $this->yaw and ($packet->pitch % 360) === $this->pitch) {
+				if($newPos->distanceSquared($this) == 0 and ($packet->yaw % 360) === $this->yaw and ($packet->pitch % 360) === $this->pitch) {
 					break;
 				}
 
