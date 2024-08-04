e88ed13d365c703cf5c93a383943a9a3e8fbd6b2
xchillz <147117006+xchillz@users.noreply.github.com>
2024-08-03 21:01:23 -0400

    feat: add singleton trait

---
+++ src/pocketmine/utils/SingletonTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\utils;
+
+trait SingletonTrait {
+
+    /** @var self|null */
+    private static $instance = null;
+
+    /**
+     * @return self
+     */
+    private static function make() {
+        return new self();
+    }
+
+    /**
+     * @return self
+     */
+    public static function getInstance() {
+        if(self::$instance === null) {
+            self::$instance = self::make();
+        }
+
+        return self::$instance;
+    }
+
+    public static function setInstance($instance) {
+        self::$instance = $instance;
+    }
+
+    public static function reset() {
+        self::$instance = null;
+    }
+
+}
