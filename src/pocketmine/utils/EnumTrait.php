71b37d5c166e8d3539266077c9f66fe3368c0b6e
xchillz <147117006+xchillz@users.noreply.github.com>
2024-09-05 23:35:18 -0400

    feat: add enumtrait

---
+++ src/pocketmine/utils/EnumTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\utils;
+
+trait EnumTrait {
+
+    protected $enumValue;
+
+    private function __construct($enumValue) {
+        $this->enumValue = $enumValue;
+    }
+
+    public static function __callStatic($methodName, $arguments) {
+        $className = get_called_class();
+
+        return new $className($methodName);
+    }
+
+    public function __toString() {
+        return $this->enumValue;
+    }
+
+}
